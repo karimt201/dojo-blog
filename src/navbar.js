560bb5d8e3d819b637e8983f5fd7e69bc5dff9f9
karim tarek <142750275+karimt201@users.noreply.github.com>
2024-07-18 02:37:43 +0300

    Adding Styles

--- src/navbar.js
+++ src/navbar.js
@@ -4,7 +4,11 @@ const navbar = () => {
             <h1>the Dojo blog</h1>
             <div className="links">
                 <a href="/">Home</a>
-                <a href="/">New Blog</a>
+                <a href="/create" style={{
+                    color : "white",
+                    backgroundColor : "#f1356d",
+                    borderRadius : '8px'
+                }}>New Blog</a>
             </div>
         </nav>
     );
