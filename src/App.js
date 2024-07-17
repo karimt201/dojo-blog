f7ad335e46fd35278fa2ffb52db95b3f0cecb8ae
karim tarek <142750275+karimt201@users.noreply.github.com>
2024-07-18 02:26:49 +0300

    Multiple Components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
 import './App.css';
-import navbar from './navbar';
+import Navbar from './navbar';
+import Home from './Home';
 
 function App() {
-  const title = 'welcome to the new blog';
-  const likes = 50;
-  // const person = {name:'ali',age:40};
-  const link = "http://www.google.com"
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
-        <h1>{title}</h1>
-        <p>liked {likes} times</p>
-        {/* <p>{person}</p> */}
-        <p>{10}</p>
-        <p>{'hello, ninjas'}</p>
-        <p>{[1,2,3,4,5]}</p>
-        <p>{Math.random() * 10}</p>
-        <a href={link}>Google Site</a>
+        <Home />
       </div>
     </div>
   );
