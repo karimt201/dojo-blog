a96a0c03a7759d0f9d13af9bf2124d24c66ffef4
karim tarek <142750275+karimt201@users.noreply.github.com>
2024-07-18 02:17:44 +0300

    Dynamic Values in Templates

---
+++ src/navbar.js
@@ -0,0 +1,13 @@
+const navbar = () => {
+    return (
+        <nav className="navbar">
+            <h1>the Dojo blog</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/">New Blog</a>
+            </div>
+        </nav>
+    );
+}
+ 
+export default navbar;
